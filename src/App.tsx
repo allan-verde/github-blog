9e0aa2c0f53cc4844eb6c37a035e74a30d5f3496
Allan Verde <allangreen2906@gmail.com>
2023-08-29 10:53:11 -0300

    feat: added lib styled-component and initial global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 const App = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  </ThemeProvider>
 )
 
 export { App }
